82033c96980ca4f172893b0d95733abc8687d1df
ilkayus <ilkay.devecioglu@gmail.com>
2023-04-08 14:47:46 +0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'anketable-client/',
+  // base: 'anketable-client/',
   plugins: [react()],
 });
